0a0c6a3c9d4ad62f8b12748da3465761ca037941
Jakob Olsen <jols19@student.aau.dk>
2022-10-08 19:05:43 +0200

    .

--- js/main.js
+++ js/main.js
@@ -33,10 +33,10 @@
         var typed_strings = $('.typed-text').text();
         var typed = new Typed('.typed-text-output', {
             strings: typed_strings.split(', '),
-            typeSpeed: 100,
+            typeSpeed: 75,
             backSpeed: 20,
             smartBackspace: false,
-            loop: true
+            loop: false
         });
     }
 
