374ef91850fc773c8a11d93a24beb6a9c51d90d0
Jakob Olsen <jols19@student.aau.dk>
2022-10-25 17:14:18 +0200

    First version

--- mail/contact.js
+++ mail/contact.js
@@ -1,7 +1,7 @@
 $(function () {
 
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
-        preventSubmit: false,
+        preventSubmit: true,
         submitError: function ($form, event, errors) {
         },
         submitSuccess: function ($form, event) {
